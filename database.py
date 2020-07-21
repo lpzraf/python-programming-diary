ea7e1a54b79b9b0c30887be09fda0d0c4ce68fdf
Rafael Lopez <lpz.raf@gmail.com>
2020-07-20 21:41:27 -0400

    It will add DB and create table function

--- database.py
+++ database.py
@@ -1,4 +1,10 @@
-entries = []
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+def create_table():
+    with connection:
+    connection.execute("CREATE TABLE entries (content TEXT,date TEXT);")
 
 def add_entry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date})
